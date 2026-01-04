9c17b28c0f0f2f77b0e50147fbb4ff93bcf89b85
Mehedi86 <abdullahalmehedi86@gmail.com>
2026-01-04 12:07:49 +0600

    added: initialization banner carosel

--- src/components/header/banner/Banner.jsx
+++ src/components/header/banner/Banner.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 export default function Banner() {
   return (
     <div>
-        This is banner
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem>...</CarouselItem>
+          <CarouselItem>...</CarouselItem>
+          <CarouselItem>...</CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   )
 }
