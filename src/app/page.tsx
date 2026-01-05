c99015af93a2ec7fa7884bf1bad24cf6807eb5a0
Mehedi86 <abdullahalmehedi86@gmail.com>
2026-01-05 11:26:39 +0600

    added: spinner animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@ import Image from "next/image";
 import Banner from "@/components/header/banner/Banner";
 import Requests from "@/components/contents/Requests";
 import WhyDonate from "@/components/contents/WhyDonate";
+import BoxSpinner from "@/components/animation/boxSpinner";
 
 export default function Home() {
   return (
-    <div className="pt-16 md:pt-20">
+    <div className="pt-16 md:pt-20 relative">
       <Banner />
       <Requests/>
       <WhyDonate />
+      <BoxSpinner position="top-10 right-20"/>
+      <BoxSpinner position="top-120 left-20"/>
     </div>
   );
 }
